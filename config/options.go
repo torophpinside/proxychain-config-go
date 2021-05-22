f9d02d0a1ca05ee79861c5e18dfe25e1f28fabd9
Carlos Toro <torophpinside@gmail.com>
2021-05-21 22:31:47 -0300

    Reorder list of proxy sources.

--- config/options.go
+++ config/options.go
@@ -12,7 +12,7 @@ func GetOptions() Options {
 	opt := Options{}
 	opt.SearchType = "list"
 	flag.IntVar(&opt.Max, "max", 9, "Max of IPs proxies")
-	flag.StringVar(&opt.Src, "source", "", "Source to read the list when search is 'list': alt,free,plus,spysone")
+	flag.StringVar(&opt.Src, "source", "", "Source to read the list when search is 'list': alt,free,nova,plus,spysone")
 	flag.Parse()
 
 	return opt
